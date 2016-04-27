4b8eecd16f5b1103acd51d41168771d05d567c10
PrestonEn <p.engstrom94@gmail.com>
2016-04-27 15:37:17 -0400

    aggregated tjs_pitchers by year

--- src/chadwick_pitchers_stats_clean.R
+++ src/chadwick_pitchers_stats_clean.R
@@ -5,6 +5,8 @@
 library(plyr)
 
 control_pitchers <- read.csv('data/stats_control_pitchers.csv')
+tjs_pitchers <- read.csv('data/stats_tjs_pitchers.csv')
+
 fips_vals <- read.csv('data/fip_constants.csv')
 
 # relavant columns
@@ -29,6 +31,20 @@ cpf$FIP = cpf$cFIP  + ((13 * cpf$HR + 3 * (cpf$BB + cpf$HBP) - 2 * cpf$SO) / (cp
 # drop fip constant
 cpf <- cpf[ , !(names(cpf) %in% c('cFIP'))]
 
-write.table(cpf, "data/stats_control_pitchers_clean.csv",
+
+
+
+# have to also aggregate tjs players if more than 1 record per year
+keeps <- c("playerID", "mlbam_id", "yearID","HR","BB","HBP", "SO","IPouts")
+tjs_pitchers <- tjs_pitchers[keeps]
+tjs_pitchers <- ddply(tjs_pitchers,
+             .(playerID, mlbam_id, yearID),
+             numcolwise(sum))
+
+
+# write tables to file
+write.table(tjs_pitchers, "data/stats_tjs_pitchers_clean.csv",
             append = F, quote = F, row.names = F, col.names = T, sep=",")
 
+write.table(cpf, "data/stats_control_pitchers_clean.csv",
+            append = F, quote = F, row.names = F, col.names = T, sep=",")
